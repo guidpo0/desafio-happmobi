9716d66dc8041e9c37f8bc416ff6c721a7b368b5
Gui Pereira <guilhermepdo1508@gmail.com>
2021-10-29 17:58:01 -0300

    Feito arquivo para instanciar banco de dados em MySQL

--- back-end/mysqlBD.sql
+++ back-end/mysqlBD.sql
@@ -1,105 +1,111 @@
-CREATE DATABASE IF NOT EXISTS `heroku_5eb1b5a5878e473`;
+CREATE DATABASE IF NOT EXISTS `happmobi`;
 
-CREATE TABLE IF NOT EXISTS heroku_5eb1b5a5878e473.Districts (
-  district_id INT PRIMARY KEY AUTO_INCREMENT,
-  district_name VARCHAR(50) NOT NULL,
-  state VARCHAR(2) NOT NULL
+DROP TABLE IF EXISTS `happmobi`.`Rents`;
+DROP TABLE IF EXISTS `happmobi`.`Users`;
+DROP TABLE IF EXISTS `happmobi`.`Address`;
+DROP TABLE IF EXISTS `happmobi`.`Cars`;
+
+CREATE TABLE IF NOT EXISTS happmobi.Address (
+  address_id INT PRIMARY KEY AUTO_INCREMENT,
+  street VARCHAR(50) NOT NULL,
+  city VARCHAR(50) NOT NULL,
+  zip VARCHAR(50) NOT NULL
 );
 
-INSERT INTO heroku_5eb1b5a5878e473.Districts(district_name, state) VALUES
-  ('Copacabana', 'RJ'),
-  ('Leblon', 'RJ'),
-  ('Ipanema', 'RJ'),
-  ('Barra da Tijuca', 'RJ');
+INSERT INTO happmobi.Address(street, city, zip) VALUES
+  ('Rua 03 de Outubro', 'São Paulo', '08090-284'),
+  ('Rua 13 de Maio', 'São Paulo', '04849-529'),
+  ('Rua 21 de Abril', 'São Paulo', '03047-000'),
+  ('Rua 21 de Setembro', 'São Paulo', '03807-020'),
+  ('Rua Abraham Lincoln', 'São Paulo', '05123-000'),
+  ('Rua Abreu Lemos', 'São Paulo', '02338-030');
 
-CREATE TABLE IF NOT EXISTS heroku_5eb1b5a5878e473.Dates (
-  date_id INT PRIMARY KEY AUTO_INCREMENT,
-  date_name VARCHAR(5) NOT NULL,
-  district_id INT NOT NULL,
-  FOREIGN KEY (district_id) REFERENCES Districts(district_id)
+CREATE TABLE IF NOT EXISTS happmobi.Users (
+  user_id INT PRIMARY KEY AUTO_INCREMENT,
+  user_email VARCHAR(50) NOT NULL,
+  user_password VARCHAR(50) NOT NULL,
+  user_role VARCHAR(50) NOT NULL,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  phone VARCHAR(50) NOT NULL,
+  address_id INT NOT NULL,
+  FOREIGN KEY (address_id) REFERENCES Address(address_id)
 );
 
-INSERT INTO heroku_5eb1b5a5878e473.Dates(date_name, district_id) VALUES
-  ('25/11', 5),
-  ('25/11', 15),
-  ('25/11', 25),
-  ('25/11', 35),
-  ('26/11', 5),
-  ('26/11', 15),
-  ('26/11', 25),
-  ('26/11', 35);
+INSERT INTO happmobi.Users(user_email, user_password, user_role, first_name, last_name, phone, address_id) VALUES
+  ('claudio@teste.com', 'claudioteste', 'admin', 'Claudio', 'Teste', '11 99999-9999',
+  (SELECT address_id FROM happmobi.Address WHERE zip = '08090-284')),
+  ('joana@teste.com', 'joanateste', 'user', 'Joana', 'Teste', '11 99999-9999',
+  (SELECT address_id FROM happmobi.Address WHERE zip = '04849-529')),
+  ('bernardo@teste.com', 'bernardoteste', 'user', 'Bernardo', 'Teste', '11 99999-9999',
+  (SELECT address_id FROM happmobi.Address WHERE zip = '03047-000')),
+  ('maria@teste.com', 'mariateste', 'user', 'Maria', 'Teste', '11 99999-9999',
+  (SELECT address_id FROM happmobi.Address WHERE zip = '03807-020')),
+  ('antonio@teste.com', 'antonioteste', 'user', 'Antonio', 'Teste', '11 99999-9999',
+  (SELECT address_id FROM happmobi.Address WHERE zip = '05123-000')),
+  ('roberta@teste.com', 'robertateste', 'user', 'Roberta', 'Teste', '11 99999-9999',
+  (SELECT address_id FROM happmobi.Address WHERE zip = '02338-030'));
 
-CREATE TABLE IF NOT EXISTS heroku_5eb1b5a5878e473.Climates (
-  climate_id INT PRIMARY KEY AUTO_INCREMENT,
-  climate_hour INT NOT NULL,
-  climate_rain FLOAT NOT NULL,
-  date_id INT NOT NULL,
-  FOREIGN KEY (date_id) REFERENCES Dates(date_id)
+CREATE TABLE IF NOT EXISTS happmobi.Cars (
+  car_id INT PRIMARY KEY AUTO_INCREMENT,
+  car_model VARCHAR(50) NOT NULL,
+  cost_hour FLOAT NOT NULL,
+  rent_status VARCHAR(50) NOT NULL
 );
 
-INSERT INTO heroku_5eb1b5a5878e473.Climates(climate_hour, climate_rain, date_id) VALUES
-  (0, 0.1, 85),
-  (3, 5.2, 85),
-  (6, 7.8, 85),
-  (9, 12.9, 85),
-  (12, 18.7, 85),
-  (15, 38.1, 85),
-  (18, 2.3, 85),
-  (21, 1, 85),
-	(0, 0.1,95),
-  (3, 5.2,95),
-  (6, 7.8,95),
-  (9, 12.9,95),
-  (12, 18.7,95),
-  (15, 38.1,95),
-  (18,2.3,95),
-  (21, 1,95),
-  (0, 0.1,105),
-  (3, 5.2,105),
-  (6, 7.8,105),
-  (9, 12.9,105),
-  (12, 18.7,105),
-  (15,38.1,105),
-  (18, 2.3,105),
-  (21, 1,105),
-	(0, 0.1,115),
-  (3, 5.2,115),
-  (6, 7.8,115),
-  (9, 12.9,115),
-  (12, 18.7,115),
-  (15, 38.1,115),
-  (18, 2.3,115),
-  (21, 1,115),
-  (0, 0.1, 125),
-  (3, 5.2, 125),
-  (6, 7.8, 125),
-  (9, 12.9, 125),
-  (12, 18.7, 125),
-  (15, 38.1, 125),
-  (18, 2.3, 125),
-  (21, 1, 125),
-  (0, 0.1, 135),
-  (3, 5.2, 135),
-  (6, 7.8, 135),
-  (9, 12.9, 135),
-  (12, 18.7, 135),
-  (15, 38.1, 135),
-  (18, 2.3, 135),
-  (21, 1, 135),
-  (0, 0.1, 145),
-  (3, 5.2, 145),
-  (6, 7.8, 145),
-  (9, 12.9, 145),
-  (12, 18.7, 145),
-  (15, 38.1, 145),
-  (18, 2.3, 145),
-  (21, 1, 145),
-  (0, 0.1, 155),
-  (3, 5.2, 155),
-  (6, 7.8, 155),
-  (9, 12.9, 155),
-  (12, 18.7, 155),
-  (15, 38.1, 155),
-  (18, 2.3, 155),
-  (21, 1, 155);
+INSERT INTO happmobi.Cars(car_model, cost_hour, rent_status) VALUES
+  ('Mini Cooper Cabrio 2.0', 43.36, 'AVAILABLE'),
+  ('T-Cross 1.0 Turbo', 19.45, 'AVAILABLE'),
+  ('Renegade 1.8', 19.54, 'AVAILABLE'),
+  ('Virtus 1.0 Turbo', 17.78, 'AVAILABLE'),
+  ('Polo MSI 1.6', 13.45, 'AVAILABLE'),
+  ('HB20 1.6', 13.89, 'AVAILABLE');
+
+CREATE TABLE IF NOT EXISTS happmobi.Rents (
+  rent_id INT PRIMARY KEY AUTO_INCREMENT,
+  car_id INT NOT NULL,
+  user_id INT NOT NULL,
+  rent_start DATETIME NOT NULL,
+  rent_end DATETIME NOT NULL,
+  total FLOAT NOT NULL,
+  FOREIGN KEY (car_id) REFERENCES Cars(car_id),
+  FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+INSERT INTO happmobi.Rents(car_id, user_id, rent_start, rent_end, total) VALUES
+  (
+    (SELECT car_id FROM happmobi.Cars WHERE car_model = 'T-Cross 1.0 Turbo'),
+    (SELECT user_id FROM happmobi.Users WHERE first_name = 'Joana'),
+    '2021-10-28 10:00:00',
+    '2021-10-30 21:00:00',
+    HOUR(TIMEDIFF(rent_end, rent_start)) * (SELECT cost_hour FROM happmobi.Cars WHERE car_model = 'T-Cross 1.0 Turbo')
+  ),
+  (
+    (SELECT car_id FROM happmobi.Cars WHERE car_model = 'Renegade 1.8'),
+    (SELECT user_id FROM happmobi.Users WHERE first_name = 'Bernardo'),
+    '2021-10-28 10:00:00',
+    '2021-10-30 21:00:00',
+    HOUR(TIMEDIFF(rent_end, rent_start)) * (SELECT cost_hour FROM happmobi.Cars WHERE car_model = 'Renegade 1.8')
+  ),
+  (
+    (SELECT car_id FROM happmobi.Cars WHERE car_model = 'Virtus 1.0 Turbo'),
+    (SELECT user_id FROM happmobi.Users WHERE first_name = 'Maria'),
+    '2021-10-28 10:00:00',
+    '2021-10-30 21:00:00',
+    HOUR(TIMEDIFF(rent_end, rent_start)) * (SELECT cost_hour FROM happmobi.Cars WHERE car_model = 'Virtus 1.0 Turbo')
+  ),
+  (
+    (SELECT car_id FROM happmobi.Cars WHERE car_model = 'HB20 1.6'),
+    (SELECT user_id FROM happmobi.Users WHERE first_name = 'Antonio'),
+    '2021-10-28 10:00:00',
+    '2021-10-30 21:00:00',
+    HOUR(TIMEDIFF(rent_end, rent_start)) * (SELECT cost_hour FROM happmobi.Cars WHERE car_model = 'HB20 1.6')
+  ),
+  (
+    (SELECT car_id FROM happmobi.Cars WHERE car_model = 'Polo MSI 1.6'),
+    (SELECT user_id FROM happmobi.Users WHERE first_name = 'Roberta'),
+    '2021-10-28 10:00:00',
+    '2021-10-30 21:00:00',
+    HOUR(TIMEDIFF(rent_end, rent_start)) * (SELECT cost_hour FROM happmobi.Cars WHERE car_model = 'Polo MSI 1.6')
+  );
    
