49a87f0e7b7cfe7e92aea34b7cef886e89db1bf8
Gui Pereira <guilhermepdo1508@gmail.com>
2021-10-31 17:32:34 -0300

    Feito CarsController

--- back-end/src/controllers/CarsController.ts
+++ back-end/src/controllers/CarsController.ts
@@ -4,67 +4,53 @@ import { CREATED_STATUS, OK_STATUS } from '../helpers/HTTPCodes';
 import { BaseCar } from '../helpers/interfaces';
 
 class CarsController {
-  public async create(
+  async create(
     req: Request, res: Response, next: NextFunction,
   ): Promise<Response | void> {
     const { carModel, costHour, rentStatus }: BaseCar = req.body;
-    const { carId, err } = await CarsService.create(
+    const car = await CarsService.create(
       { carModel, costHour, rentStatus },
     );
-    if (err) return next(err);
-    return res.status(CREATED_STATUS).json({
-      carId, carModel, costHour,
-    });
+    if (car.err) return next(car.err);
+    return res.status(CREATED_STATUS).json(car);
   }
 
-  // public async getAll(
-  //   req: Request, res: Response,
-  // ): Promise<Response> {
-  //   const cars = await CarsService.getAll();
-  //   return res.status(OK_STATUS).json({ cars });
-  // }
+  async getAll(
+    req: Request, res: Response,
+  ): Promise<Response> {
+    const cars = await CarsService.getAll();
+    return res.status(OK_STATUS).json({ cars });
+  }
 
-  // public async getById(
-  //   req: Request, res: Response, next: NextFunction,
-  // ): Promise<Response> {
-  //   const { id } = req.params;
-  //   const car = await CarsService.getById(id);
-  //   const { err } = car;
-  //   if (err) return next(err);
-  //   return res.status(OK_STATUS).json(car);
-  // }
+  async getById(
+    req: Request, res: Response, next: NextFunction,
+  ): Promise<Response | void> {
+    const { id } = req.params;
+    const car = await CarsService.getById(Number(id));
+    if (car.err) return next(car.err);
+    return res.status(OK_STATUS).json(car);
+  }
 
-  // public async remove(
-  //   req: Request, res: Response, next: NextFunction,
-  // ): Promise<Response> {
-  //   const { id } = req.params;
-  //   const carRemoved = await CarsService.remove(id);
-  //   const { err } = carRemoved;
-  //   if (err) return next(err);
-  //   return res.status(OK_STATUS).json(carRemoved);
-  // }
+  async remove(
+    req: Request, res: Response, next: NextFunction,
+  ): Promise<Response | void> {
+    const { id } = req.params;
+    const carRemoved = await CarsService.remove(Number(id));
+    if (carRemoved.err) return next(carRemoved.err);
+    return res.status(OK_STATUS).json(carRemoved);
+  }
 
-  // public async update(
-  //   req: Request, res: Response, next: NextFunction,
-  // ): Promise<Response> {
-  //   const { carModel, costHour, status } = req.body;
-  //   const { id } = req.params;
-  //   const { carId, err } = await CarsService.update({
-  //     carId: id, carModel, costHour, status,
-  //   });
-  //   if (err) return next(err);
-  //   return res.status(OK_STATUS).json({
-  //     carId, carModel, costHour, status,
-  //   });
-  // }
-
-  // public constructor() {
-  //   this.create = this.create.bind(this);
-  //   this.getAll = this.getAll.bind(this);
-  //   this.getById = this.getById.bind(this);
-  //   this.remove = this.remove.bind(this);
-  //   this.update = this.update.bind(this);
-  // }
+  async update(
+    req: Request, res: Response, next: NextFunction,
+  ): Promise<Response | void> {
+    const { carModel, costHour, rentStatus }: BaseCar = req.body;
+    const { id } = req.params;
+    const carUpdated = await CarsService.update({
+      carId: Number(id), carModel, costHour, rentStatus,
+    });
+    if (carUpdated.err) return next(carUpdated.err);
+    return res.status(OK_STATUS).json(carUpdated);
+  }
 }
 
 export default new CarsController();
