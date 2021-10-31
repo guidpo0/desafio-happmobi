e38def82b145b10af6890f6331d92e7f3fc07a7b
Gui Pereira <guilhermepdo1508@gmail.com>
2021-10-31 11:53:06 -0300

    feito UserModel

--- back-end/src/helpers/interfaces.ts
+++ back-end/src/helpers/interfaces.ts
@@ -26,3 +26,20 @@ export interface Rent extends BaseRent {
   rentId: number,
   err?: ResponseError,
 }
+
+export interface BaseUser {
+  userEmail: string,
+  userPassword: string,
+  userRole: string,
+  firstName: string,
+  lastName: string,
+  phone: string,
+  street: string,
+  city: string,
+  zip: string,
+}
+
+export interface User extends BaseUser {
+  userId: number,
+  err?: ResponseError,
+}
--- back-end/src/models/UsersModel.ts
+++ back-end/src/models/UsersModel.ts
@@ -1,63 +1,84 @@
-// import mysqlConnection from '../connections/mysqlServer';
+import mysqlConnection from '../connections/mysqlServer';
+import { BaseUser, User } from '../helpers/interfaces';
 
-// class UsersModel {
-//   public async create({ dateName, districtId }) {
-//     const [{ insertId }] = await mysqlConnection.execute(
-//       'INSERT INTO heroku_5eb1b5a5878e473.Dates (date_name, district_id) VALUES (?,?)',
-//       [dateName, districtId],
-//     );
-//     return { dateId: insertId };
-//   }
+class UsersModel {
+  async create({
+    userEmail, userPassword, userRole, firstName, lastName, phone, street, city, zip,
+  }: BaseUser): Promise<User> {
+    const [{ insertId }] = await mysqlConnection.execute(
+      'INSERT INTO happmobi.Users (user_email, user_password, user_role, first_name, last_name, phone, street, city, zip,) VALUES (?,?,?,?,?,?,?,?,?)',
+      [userEmail, userPassword, userRole, firstName, lastName, phone, street, city, zip],
+    );
+    return {
+      userId: insertId,
+      userEmail,
+      userPassword,
+      userRole,
+      firstName,
+      lastName,
+      phone,
+      street,
+      city,
+      zip,
+    };
+  }
 
-//   public async getAll() {
-//     const [dates] = await mysqlConnection.execute('SELECT * FROM heroku_5eb1b5a5878e473.Dates');
-//     return dates.map(({
-//       date_id: dateId,
-//       date_name: dateName,
-//       district_id: districtId,
-//     }) => ({ dateId, dateName, districtId }));
-//   }
+  async getAll(): Promise<User[]> {
+    const [users] = await mysqlConnection.execute(
+      'SELECT * FROM happmobi.Users',
+    );
+    return users.map(({
+      user_id: userId,
+      user_email: userEmail,
+      user_password: userPassword,
+      user_role: userRole,
+      first_name: firstName,
+      last_name: lastName,
+      phone,
+      street,
+      city,
+      zip,
+    }) => ({
+      userId, userEmail, userPassword, userRole, firstName, lastName, phone, street, city, zip,
+    }));
+  }
 
-//   public async getById(id) {
-//     const [dates] = await mysqlConnection.execute(
-//       'SELECT * FROM heroku_5eb1b5a5878e473.Dates WHERE date_id = ?', [id],
-//     );
-//     return {
-//       dateId: dates[0].date_id,
-//       dateName: dates[0].date_name,
-//       districtId: dates[0].district_id,
-//     };
-//   }
+  async getById(id: number): Promise<User> {
+    const [user] = await mysqlConnection.execute(
+      'SELECT * FROM happmobi.Users WHERE user_id = ?', [id],
+    );
+    return {
+      userId: user[0].user_id,
+      userEmail: user[0].user_email,
+      userPassword: user[0].user_password,
+      userRole: user[0].user_role,
+      firstName: user[0].first_name,
+      lastName: user[0].last_name,
+      phone: user[0].phone,
+      street: user[0].street,
+      city: user[0].city,
+      zip: user[0].zip,
+    };
+  }
 
-//   public async remove(id) {
-//     const [dates] = await mysqlConnection.execute(
-//       'SELECT * FROM heroku_5eb1b5a5878e473.Dates WHERE date_id = ?', [id],
-//     );
-//     return {
-//       dateId: dates[0].date_id,
-//       dateName: dates[0].date_name,
-//       districtId: dates[0].district_id,
-//     };
-//   }
+  async remove(id: number): Promise<User> {
+    const user = await this.getById(id);
+    await mysqlConnection.execute(
+      'DELETE FROM happmobi.Users WHERE user_id = ?', [id],
+    );
+    return user;
+  }
 
-//   public async update(id) {
-//     const [dates] = await mysqlConnection.execute(
-//       'SELECT * FROM heroku_5eb1b5a5878e473.Dates WHERE date_id = ?', [id],
-//     );
-//     return {
-//       dateId: dates[0].date_id,
-//       dateName: dates[0].date_name,
-//       districtId: dates[0].district_id,
-//     };
-//   }
+  public async update({
+    userId, userEmail, userPassword, userRole, firstName, lastName, phone, street, city, zip,
+  }: User): Promise<User> {
+    const user = await this.getById(userId);
+    await mysqlConnection.execute(
+      'UPDATE happmobi.Rents SET user_email = ?, user_password = ?, user_role = ?, first_name = ?, last_name = ?, phone = ?, street = ?, city = ?, zip = ? WHERE user_id = ?',
+      [userEmail, userPassword, userRole, firstName, lastName, phone, street, city, zip, userId],
+    );
+    return user;
+  }
+}
 
-//   constructor() {
-//     this.create = this.create.bind(this);
-//     this.getAll = this.getAll.bind(this);
-//     this.getById = this.getById.bind(this);
-//     this.remove = this.remove.bind(this);
-//     this.update = this.update.bind(this);
-//   }
-// }
-
-// export default new UsersModel();
+export default new UsersModel();
