7732bb58d96cab6cd4c882ee199745ef05491dc1
Gui Pereira <guilhermepdo1508@gmail.com>
2021-11-01 19:44:08 -0300

    Feita estrutura inicial

---
+++ front-end/src/app/components/cars-available/cars-available.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cars-available',
+  templateUrl: './cars-available.component.html',
+  styleUrls: ['./cars-available.component.css']
+})
+export class CarsAvailableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
